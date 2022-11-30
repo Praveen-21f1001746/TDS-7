08086e7b7b6084cba63a109422a7270a37092b92
Praveen-21f1001746 <119427099+Praveen-21f1001746@users.noreply.github.com>
2022-11-30 20:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
-st.title("Greater of Three Numbers:")
+st.title("Largest of three Numbers:")
 
 a=st.number_input("Enter First Number:")
 b=st.number_input("Enter Second Number:")
 c=st.number_input("Enter Third Number:")
 
 if (a>b) and (a>c):
-  st.write("a is greater")
+  st.write("First Number is Largest")
 elif (b>a) and (b>c):
-  st.write("b is greater")
+  st.write("Second Number is Largest")
 else:
-  st.write("c is greater")
+  st.write("Third Number is Largest")
